c6c2674e824e91da149254cf58a13af626bcca65
Brandon Taylor <btaylor66@gmail.com>
2017-06-01 23:21:37 -0700

    converted to using a class for board

--- nqueens.py
+++ nqueens.py
@@ -1,136 +1,118 @@
 #!/usr/bin/env python
 
 import logging
-import os
-n=4
 
-# Create object? n, board, ?
+class Board:
+
+    def __init__(self,n):
+        self.n = n     # Size of board and number of queens to be placed
+        self.board = [ [" "] * n for unused in range(n)]
+
+    def print_board(self):
+        for x in self.board:
+            for y in x:
+                print('|', y, '|', end='')
+            print()
+
+    def place_queen(self,x,y):
+        try:
+            self.board[x][y] = "Q"
+        except IndexError as err:
+            print("Error placing queen in ",x,",",y," : ", err)
+    def check_queen(self, x, y):
+
+        count = 0
+
+        for i in range(self.n):
+            if self.board[i][y] == "Q" and i != x:
+                # print("X FAIL!!",i," ",y)
+                self.board[i][y] = "X"
+                count += 1
+            if self.board[x][i] == "Q" and i != y:
+                # print("Y FAIL!!",x," ",i)
+                self.board[x][i] = "X"
+                count += 1
+
+        # checking diagonal
+        if x <= y:
+            start_x = 0
+            start_y = y - x
+        else:
+            start_x = x - y
+            start_y = 0
+
+        while start_x < self.n and start_y < self.n:
+            if self.board[start_x][start_y] == "Q" and start_x != x:
+                #print("Diag FAIL 1 !!", start_x, " ", start_y)
+                self.board[start_x][start_y] = "X"
+                count += 1
+            start_x += 1
+            start_y += 1
+
+        if x <= y:
+            start_x = x - (self.n - y)
+            start_y = self.n
+        else:
+            start_x = 0
+            start_y = y + x
+
+        while start_x < self.n and start_y >= 0 and start_y < self.n and x >= 0:
+            if self.board[start_x][start_y] == "Q" and start_x != x:
+                print("Diag FAIL 2!!", start_x, " ", start_y)
+                self.board[start_x][start_y] = "X"
+                count += 1
+            start_x += 1
+            start_y -= 1
+        if count == 0:
+            # print("Verified")
+            return 0
+        else:
+            # print("Found ",count," misplaced queens")
+            return count
+
+    def verify_board(self):
+        x = 0
+        y = 0
+        queens_failed = 0
+
+        # Checking X and Y
+        for row in self.board:
+            for cell in row:
+                if cell == "Q":
+                    # print("Found One! X:",x," Y:",y)
+                    queens_failed += self.check_queen(x,y)
+                y += 1
+            y = 0
+            x += 1
+        # if queens_failed == 0:
+        #    print("Verified")
+        #else:
+        #    print(queens_failed, "Queens misplaced")
+
+        return queens_failed
+
+
 
 log = logging.getLogger(__name__)
 
-def initialize_board(n):
-	print("Creating board")
-	board = [ [" "] * n for unused in range(n)]
-	return board
 
-def check_queen(x,y,board):
-    n = len(board)
-    count = 0
-    print(board[x][y])
-    for i in range(len(board)):
-        if board[i][y] == "Q" and i != x:
-            # print("X FAIL!!",i," ",y)
-            board[i][y] = "X"
-            count += 1
-        if board[x][i] == "Q" and i !=y:
-            # print("Y FAIL!!",x," ",i)
-            board[x][i] = "X"
-            count += 1
 
-    # checking diagonal
-    if x <= y:
-        start_x = 0
-        start_y = y - x
-    else:
-        start_x = x - y
-        start_y = 0
 
-    while start_x < n and start_y < n:
-        if board[start_x][start_y] == "Q" and start_x != x:
-            print("Diag FAIL 1 !!",start_x," ",start_y)
-            board[start_x][start_y] = "X"
-            count += 1
-        start_x += 1
-        start_y += 1
 
-    if x <= y:
-        start_x = x - ( n- y )
-        start_y = n
-    else:
-        start_x = 0
-        start_y = y + x
-
-    while start_x < n and start_y >= 0 and start_y < n and x >= 0:
-        if board[start_x][start_y] == "Q" and start_x != x:
-            print("Diag FAIL 2!!",start_x," ",start_y)
-            board[start_x][start_y] = "X"
-            count += 1
-        start_x += 1
-        start_y -= 1
-    if count == 0:
-        print("Verified")
-    else:
-        print("Found ",count," misplaced queens")
-
-def locate_queens(board):
-    x = 0
-    y = 0
-
-    # Checking X and Y
-    for row in board:
-        for cell in row:
-            if cell == "Q":
-                print("Found One! X:",x," Y:",y)
-                check_queen(x,y,board)
-            y += 1
-        y = 0
-        x += 1
-
-def print_board(board):
-    for x in board:
-        for y in x:
-            print('|',y,'|',end='')
-        print()
-
-def  place_queen(x,y,board):
-    board[x][y] = "Q"
-    return board
 
 def main():
-    board_size = 4
-    board = initialize_board(board_size)
 
-    board = place_queen(0, 0, board)
-    board = place_queen(3, 3, board)
-    locate_queens(board)
-    print_board(board)
+    size4 = Board(4)
+    size4.place_queen(0, 0)
+    size4.place_queen(0, 5)
+    size4.verify_board()
     print()
+    size5 = Board(4)
+    size5.place_queen(3, 0)
+    size5.place_queen(3, 3)
+    size5.print_board()
+    size5.verify_board()
+    size5.print_board()
 
-    board = initialize_board(board_size)
-    board = place_queen(0, 3, board)
-    locate_queens(board)
-    print_board(board)
-    print()
-
-    board = initialize_board(board_size)
-    board = place_queen(3, 3, board)
-    board = place_queen(3, 0, board)
-    locate_queens(board)
-    print_board(board)
-    print()
-
-    board = initialize_board(board_size)
-    board = place_queen(0, 3, board)
-    board = place_queen(3, 0, board)
-    locate_queens(board)
-    print_board(board)
-    print()
-
-    board = initialize_board(board_size)
-    board = place_queen(1, 1, board)
-    board = place_queen(3, 3, board)
-    board = place_queen(0, 2, board)
-    locate_queens(board)
-    print_board(board)
-
-    print()
-
-    board = initialize_board(board_size)
-    board = place_queen(0, 2, board)
-    board = place_queen(1, 0, board)
-    board = place_queen(2, 3, board)
-    board = place_queen(3, 1, board)
-    locate_queens(board)
-    print_board(board)
 if __name__ == '__main__':
 	main()
