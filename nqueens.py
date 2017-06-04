8acb1c1b3cd5e6e35b4d0df1c8410bab7c8bcd28
Brandon Taylor <btaylor66@gmail.com>
2017-06-04 13:30:46 -0700

    Got the solver working, with backtracking. Also got the test cases working. I also fixed a bug in how the diagonals were checked.

--- nqueens.py
+++ nqueens.py
@@ -1,36 +1,60 @@
 #!/usr/bin/env python
 
 import logging
+#logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.ERROR)
 
 class Board:
 
     def __init__(self,n):
         self.n = n     # Size of board and number of queens to be placed
-        self.board = [ [" "] * n for unused in range(n)]
+        self.count = 0 # Number of queens placed
+        self.board = [ ["_"] * n for unused in range(n)]
+        self.solutions = {}
 
     def print_board(self):
         for x in self.board:
             for y in x:
-                print('|', y, '|', end='')
+                #print('|', y, '|', end='')
+                print(y," ", end='')
             print()
 
     def place_queen(self,x,y):
+        logging.debug("place_queen %i,%i", x, y)
         try:
-            self.board[x][y] = "Q"
+           self.board[x][y] = "Q"
         except IndexError as err:
-            print("Error placing queen in ",x,",",y," : ", err)
-    def check_queen(self, x, y):
+            logging.error("Error placing queen %i,%i: %s", x, y,err)
+            return
+        self.count += 1
 
+    def check_n_place_queen(self,x,y):
+        logging.debug("check_n_place_queen %i,%i", x,y)
+        try:
+            if self.check_queen(x,y) == 0:
+                logging.debug("placing queen at %i,%i", x,y)
+                self.board[x][y] = "Q"
+            else:
+                return -1
+        except IndexError as err:
+            logging.error("Error placing queen %i,%i: %s", x, y,err)
+            return -1
+        self.count += 1
+        return 0
+
+    def check_queen(self, x, y):
+        logging.debug("check_queen %i,%i", x, y)
+        #logging.debug(self.print_board())
         count = 0
 
         for i in range(self.n):
             if self.board[i][y] == "Q" and i != x:
-                # print("X FAIL!!",i," ",y)
-                self.board[i][y] = "X"
+                logging.debug("X FAIL!! %i,%i ",i,y)
+                #self.board[i][y] = "X"
                 count += 1
             if self.board[x][i] == "Q" and i != y:
-                # print("Y FAIL!!",x," ",i)
-                self.board[x][i] = "X"
+                logging.debug("Y FAIL!! %i,%i",x,i)
+                #self.board[x][i] = "X"
                 count += 1
 
         # checking diagonal
@@ -42,35 +66,36 @@ class Board:
             start_y = 0
 
         while start_x < self.n and start_y < self.n:
-            if self.board[start_x][start_y] == "Q" and start_x != x:
-                #print("Diag FAIL 1 !!", start_x, " ", start_y)
-                self.board[start_x][start_y] = "X"
+            if self.board[start_x][start_y] == "Q" and start_x != x and start_y != y:
+                logging.debug("Diag FAIL 1 !! %i,%i", start_x, start_y)
+                #self.board[start_x][start_y] = "X"
                 count += 1
             start_x += 1
             start_y += 1
 
         if x <= y:
-            start_x = x - (self.n - y)
-            start_y = self.n
+            start_x = x - ((self.n - 1) - y)    # board offset is 0 so need to decrement board size by 1
+            start_y = self.n -1 # board offset is 0 so need to decrement board size by 1
         else:
             start_x = 0
             start_y = y + x
 
         while start_x < self.n and start_y >= 0 and start_y < self.n and x >= 0:
             if self.board[start_x][start_y] == "Q" and start_x != x:
-                print("Diag FAIL 2!!", start_x, " ", start_y)
-                self.board[start_x][start_y] = "X"
+                logging.debug("Diag FAIL 2!! %i,%i", start_x, start_y)
+                #self.board[start_x][start_y] = "X"
                 count += 1
             start_x += 1
             start_y -= 1
         if count == 0:
-            # print("Verified")
+            logging.debug("Verified %i,%i", x, y)
             return 0
         else:
-            # print("Found ",count," misplaced queens")
+            logging.debug("Found %i misplaced queens", count)
             return count
 
     def verify_board(self):
+        logging.debug("verify_board")
         x = 0
         y = 0
         queens_failed = 0
@@ -79,7 +104,7 @@ class Board:
         for row in self.board:
             for cell in row:
                 if cell == "Q":
-                    # print("Found One! X:",x," Y:",y)
+                    logging.debug("Found One! %i,%i",x,y)
                     queens_failed += self.check_queen(x,y)
                 y += 1
             y = 0
@@ -91,28 +116,45 @@ class Board:
 
         return queens_failed
 
-
-
 log = logging.getLogger(__name__)
 
 
+def solver(board,x,y):
+    logging.debug("solver %i,%i", x, y)
+    row = x
+    col = 0
+
+    while board.count < board.n:
+        logging.debug("solver:while %i < %i", board.count, board.n)
+        placed_queen = board.check_n_place_queen(row,col)
+
+        if placed_queen == 0:
+            logging.info("Placed queen# %i at position: %i,%i ", board.count,row,col)
+            ret_val = solver(board,row+1,col)
+            if ret_val == -1:
+                logging.debug("unable to place queen on row: %i", row+1)
+                logging.debug("Removing queen: %i,%i",row,col)
+                board.board[row][col] = "_"
+                board.count -= 1
+                col += 1
+                if col == board.n:
+                    return -1
+            else:
+                col = 0
+        else:
+            col += 1
+            if col == board.n:
+                return -1
 
 
-
+    if board.count == board.n:
+        return
 
 def main():
 
-    size4 = Board(4)
-    size4.place_queen(0, 0)
-    size4.place_queen(0, 5)
-    size4.verify_board()
-    print()
-    size5 = Board(4)
-    size5.place_queen(3, 0)
-    size5.place_queen(3, 3)
-    size5.print_board()
-    size5.verify_board()
-    size5.print_board()
+    size4 = Board(100)
+    solver(size4,0,0)
+    size4.print_board()
 
 if __name__ == '__main__':
 	main()
--- test.py
+++ test.py
@@ -5,6 +5,9 @@ import nqueens
 
 class TestBoardMethods(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     def test_initialize(self):
         test = nqueens.Board(4)
         self.assertEqual(test.n, 4, 'Incorrect Board Size')
@@ -13,25 +16,32 @@ class TestBoardMethods(unittest.TestCase):
         test = nqueens.Board(4)
         test.place_queen(0,0)
         test.place_queen(0,2)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
 
     def test_vertical_fail(self):
         test = nqueens.Board(4)
         test.place_queen(0,1)
         test.place_queen(3,1)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
 
     def test_diagonal_fail(self):
         test = nqueens.Board(4)
         test.place_queen(0,0)
         test.place_queen(3,3)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
+
+    def test_diagonal_fail2(self):
+        test = nqueens.Board(4)
+        test.place_queen(0,3)
+        test.place_queen(3,0)
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
+
 
     def test_x_out_of_scope(self):
         test = nqueens.Board(4)
         test.place_queen(0,0)
         test.place_queen(5,3)
-        self.assertEqual(test.verify_board(),'Error placing queen in  0 , 5  :  list assignment index out of range','Expecting Error placing Queen')
+        self.assertEqual(test.verify_board(),0,'ERROR:root:Error placing queen 5,3: list index out of range',)
 
     def test_correct_size4(self):
         test = nqueens.Board(4)
@@ -45,19 +55,19 @@ class TestBoardMethods(unittest.TestCase):
         test = nqueens.Board(1000)
         test.place_queen(0,0)
         test.place_queen(0,962)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
 
     def test_vertical_fail_size1000(self):
         test = nqueens.Board(1000)
         test.place_queen(0,1)
         test.place_queen(850,1)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
 
     def test_diagonal_fail_size1000(self):
         test = nqueens.Board(1000)
         test.place_queen(0,0)
         test.place_queen(999,999)
-        self.assertEqual(test.verify_board(),1, 'Looking for 1 Queen misplaced')
+        self.assertEqual(test.verify_board(),2, 'Looking for 2 Queen misplaced')
 
 if __name__ == '__main__':
     unittest.main()
